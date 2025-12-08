41204216ee452bc932c0e5098eed2b2b7a25bcc6
beshakimi <w.user2022@gmail.com>
2025-12-08 21:04:27 +0430

    un read notification count

--- src/components/layout/sidebar/SidebarItem.tsx
+++ src/components/layout/sidebar/SidebarItem.tsx
@@ -3,6 +3,9 @@ import Link from 'next/link';
 import { useState } from 'react';
 import clsx from 'clsx';
 import SidebarSubmenu from './SidebarSubmenu';
+import { useApiGet } from '@/hooks/useApi';
+import { NOTIFICATION } from '@/endpoints/notification';
+import { NotificationListResponse } from '@/types/notification/notifications';
 
 interface SidebarItemProps {
   title: string;
@@ -15,18 +18,25 @@ interface SidebarItemProps {
 const SidebarItem = ({ title, icon: Icon, link, submenu, collapsed }: SidebarItemProps) => {
   const [open, setOpen] = useState(false);
 
+  // فقط برای اعلانات
+  const { data: notifications } = useApiGet<NotificationListResponse>(
+    'notifications',
+    NOTIFICATION.list
+  );
+
+  const unreadCount = notifications?.data.filter(n => !n.is_read).length ?? 0;
+
   if (submenu) {
     return (
       <div>
         <button
           onClick={() => setOpen(!open)}
           className={clsx(
-            `flex items-center justify-between p-2 rounded-lg text-gray-700 hover:bg-primary-100 hover:text-primary-700 transition ${collapsed ? "w-fit" : "w-full"
+            `flex items-center justify-between p-2 rounded-lg text-gray-700 hover:bg-primary-100 hover:text-primary-700 transition ${collapsed ? 'w-fit' : 'w-full'
             }`,
-            open && "bg-primary-100 text-primary-700"
+            open && 'bg-primary-100 text-primary-700'
           )}
         >
-
           <div className="flex items-center space-x-2">
             <Icon size={20} />
             {!collapsed && <span>{title}</span>}
@@ -51,10 +61,22 @@ const SidebarItem = ({ title, icon: Icon, link, submenu, collapsed }: SidebarIte
   return (
     <Link
       href={link!}
-      className={` ${collapsed?'w-fit':'w-full'} flex items-center space-x-2 p-2 rounded-lg text-gray-700 hover:bg-primary-100 hover:text-primary-700 transition`}
+      className={`${collapsed ? 'w-fit' : 'w-full'
+        } flex items-center space-x-2 p-2 rounded-lg text-gray-700 hover:bg-primary-100 hover:text-primary-700 transition`}
     >
       <Icon size={20} />
-      {!collapsed && <span>{title}</span>}
+      {!collapsed && (
+        <div className="flex items-center space-x-2 relative w-full">
+          <span>{title}</span>
+          {title === 'اعلانات' && unreadCount > 0 && (
+            <div className="absolute top-0 left-0 flex items-center justify-center min-w-5 min-h-5 max-w-5 max-h-5 text-xs font-bold  text-white bg-red-500 rounded-full animate-pulse">
+              <span className='pt-[2px]'>
+                {unreadCount}
+              </span>
+            </div>
+          )}
+        </div>
+      )}
     </Link>
   );
 };
